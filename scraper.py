e72513e4c17be4d7c73331991c5544784fa7396b
Adham_Emam <mrado372002@gmail.com>
2024-06-13 03:57:45 +0300

    adjustments

--- scraper.py
+++ scraper.py
@@ -2,23 +2,23 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import time
 
 TWITTER_ACCOUNTS = [
-    "https://twitter.com/Mr_Derivatives",
-    "https://twitter.com/warrior_0719",
-    "https://twitter.com/ChartingProdigy",
-    "https://twitter.com/allstarcharts",
-    "https://twitter.com/yuriymatso",
-    "https://twitter.com/TriggerTrades",
-    "https://twitter.com/AdamMancini4",
-    "https://twitter.com/CordovaTrades",
-    "https://twitter.com/Barchart",
-    "https://twitter.com/RoyLMattox"
+    "https://x.com/Mr_Derivatives",
+    "https://x.com/warrior_0719",
+    "https://x.com/ChartingProdigy",
+    "https://x.com/allstarcharts",
+    "https://x.com/yuriymatso",
+    "https://x.com/TriggerTrades",
+    "https://x.com/AdamMancini4",
+    "https://x.com/CordovaTrades",
+    "https://x.com/Barchart",
+    "https://x.com/RoyLMattox"
 ]
 TICKER = "$TSLA"
 INTERVAL = 15  # Time interval in minutes
-
 
 
 chrome_options = Options()
@@ -26,6 +26,12 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--disable-logging")
+chrome_options.add_argument("--log-level=3")
+
+# Set logging preferences
+caps = DesiredCapabilities.CHROME
+caps['goog:loggingPrefs'] = {'browser': 'OFF'}
 
 
 # Set up the selenium driver
@@ -46,15 +52,24 @@ def scrape_mentions(account, ticker):
     # get all tweets
     tweets = driver.find_elements(By.CSS_SELECTOR, "article div[lang]")
 
-    count = sum(1 for tweet in tweets if TICKER in tweet.text)
+    counter = 0
 
-    return count
+    for tweet in tweets:
+        if TICKER in tweet.text:
+            counter += 1
+
+    return counter
 
 
 def main(twitter_accounts, ticker, interval):
-    total_mentions = sum(scrape_mentions(account, TICKER)
-                         for account in TWITTER_ACCOUNTS)
-    print(f"'{TICKER}' was mentioned '{total_mentions}' times in the last '{INTERVAL}' minutes.")
+    total_mentions = []
+    for account in TWITTER_ACCOUNTS:
+        username = account.split('/')[-1]
+        print(f'Counting Tickers on {username} account...')
+        total_mentions.append(scrape_mentions(account, TICKER))
+        print('Done')
+
+    print(f"'{TICKER}' was mentioned '{sum(total_mentions)}' times in the last '{INTERVAL}' minutes.")
 
     # Re-run the script at the specified interval
     time.sleep(INTERVAL * 60)
